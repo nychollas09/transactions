b895de2a12043dae562c220d40c587573fa80a8b
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-02 15:39:21 -0300

    modifier

--- transactions/src/main/java/br/com/transactions/service/TransactionService.java
+++
@@ -1,7 +0,0 @@
-package br.com.transactions.service;
-
-import br.com.transactions.dto.TransactionDataTransferObject;
-
-public interface TransactionService {
-    void save(TransactionDataTransferObject requestDTO);
-}
