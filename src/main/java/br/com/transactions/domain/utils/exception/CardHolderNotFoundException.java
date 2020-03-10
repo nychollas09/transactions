b9ec4a0840d36aaa9ff4a1585cb9646487918e35
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-10 11:22:25 -0300

    adicionar response status aos exceptions not_found

--- src/main/java/br/com/transactions/domain/utils/exception/CardHolderNotFoundException.java
+++ src/main/java/br/com/transactions/domain/utils/exception/CardHolderNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.transactions.domain.utils.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class CardHolderNotFoundException extends RuntimeException {
 
   private static final long serialVersionUID = 1421424971858326175L;
--- src/main/java/br/com/transactions/domain/utils/exception/TransactionTypeNotFoundException.java
+++ src/main/java/br/com/transactions/domain/utils/exception/TransactionTypeNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.transactions.domain.utils.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class TransactionTypeNotFoundException extends RuntimeException {
 
   private static final long serialVersionUID = 9034731086851462672L;
