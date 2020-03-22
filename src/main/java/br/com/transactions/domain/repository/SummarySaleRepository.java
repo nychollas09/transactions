b32d53d6f623c15fce660eb544e69d83740fe7a8
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-22 12:25:55 -0300

    adicionando specification

--- src/main/java/br/com/transactions/domain/repository/SummarySaleRepository.java
+++ src/main/java/br/com/transactions/domain/repository/SummarySaleRepository.java
@@ -4,9 +4,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import br.com.transactions.domain.model.SummarySale;
 
-public interface SummarySaleRepository extends JpaRepository<SummarySale, UUID> {
+public interface SummarySaleRepository
+    extends JpaRepository<SummarySale, UUID>, JpaSpecificationExecutor<SummarySale> {
   Optional<SummarySale> findByNumberSummarySale(Long numberSummarySale);
 }
