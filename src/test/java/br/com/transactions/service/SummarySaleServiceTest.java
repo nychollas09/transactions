37ca71f644af0445eeff65365ffa3d1598a5f04d
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-22 12:51:11 -0300

    agora o construtor recebe 2 parametros, corrigindo

--- src/test/java/br/com/transactions/service/SummarySaleServiceTest.java
+++ src/test/java/br/com/transactions/service/SummarySaleServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import br.com.transactions.domain.model.SummarySale;
 import br.com.transactions.domain.repository.SummarySaleRepository;
+import br.com.transactions.filter.SummarySaleSpecification;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SummarySaleServiceTest {
@@ -18,12 +19,15 @@ public class SummarySaleServiceTest {
   @Mock
   private SummarySaleRepository repository;
 
+  @Mock
+  private SummarySaleSpecification specification;
+
   private SummarySaleServiceImpl service;
   private List<SummarySale> summarys;
 
   @Before
   public void berfore() {
-    this.service = new SummarySaleServiceImpl(repository);
+    this.service = new SummarySaleServiceImpl(repository, specification);
 
     SummarySale summarySaleOne = new SummarySale(BigDecimal.valueOf(98.00).setScale(2),
         BigDecimal.valueOf(100.00).setScale(2), Double.parseDouble("0.02"),
