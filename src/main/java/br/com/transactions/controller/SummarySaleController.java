6e93be90ce7784ff40e3400eaee175f5b26eaa30
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-22 12:12:40 -0300

    correcao, api deveria estar chamando a interface

--- src/main/java/br/com/transactions/controller/SummarySaleController.java
+++ src/main/java/br/com/transactions/controller/SummarySaleController.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.transactions.dto.SummarySaleDataTransferObject;
-import br.com.transactions.service.SummarySaleServiceImpl;
+import br.com.transactions.service.SummarySaleService;
 
 @RestController
 @RequestMapping("api/summary-sale")
 public class SummarySaleController {
 
   @Autowired
-  private SummarySaleServiceImpl service;
+  private SummarySaleService service;
 
   @GetMapping("find/{number}")
   public ResponseEntity<SummarySaleDataTransferObject> findByNumber(
       @PathVariable(value = "number") String number) {
     return ResponseEntity.ok(service.findByNumberSummarySale(number));
   }
-  
+
 }
