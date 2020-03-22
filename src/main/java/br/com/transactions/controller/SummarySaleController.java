85ec94190dad91e222e82bd1832f17158afeeac7
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-22 12:07:19 -0300

    importacoes

--- src/main/java/br/com/transactions/controller/SummarySaleController.java
+++ src/main/java/br/com/transactions/controller/SummarySaleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.transactions.resource.SummarySaleResource;
+import br.com.transactions.dto.SummarySaleDataTransferObject;
 import br.com.transactions.service.SummarySaleServiceImpl;
 
 @RestController
@@ -17,9 +17,9 @@ public class SummarySaleController {
   private SummarySaleServiceImpl service;
 
   @GetMapping("find/{number}")
-  public ResponseEntity<SummarySaleResource> findByNumber(
+  public ResponseEntity<SummarySaleDataTransferObject> findByNumber(
       @PathVariable(value = "number") String number) {
     return ResponseEntity.ok(service.findByNumberSummarySale(number));
   }
-
+  
 }
