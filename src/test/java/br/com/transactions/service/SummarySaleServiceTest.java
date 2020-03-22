0ff28632f3aabbe8790b6df2501b7c25a2626923
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-22 12:23:18 -0300

    teste para o findAll que acabei de criar

---
+++ src/test/java/br/com/transactions/service/SummarySaleServiceTest.java
@@ -0,0 +1,50 @@
+package br.com.transactions.service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import br.com.transactions.domain.model.SummarySale;
+import br.com.transactions.domain.repository.SummarySaleRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SummarySaleServiceTest {
+
+  @Mock
+  private SummarySaleRepository repository;
+
+  private SummarySaleServiceImpl service;
+  private List<SummarySale> summarys;
+
+  @Before
+  public void berfore() {
+    this.service = new SummarySaleServiceImpl(repository);
+
+    SummarySale summarySaleOne = new SummarySale(BigDecimal.valueOf(98.00).setScale(2),
+        BigDecimal.valueOf(100.00).setScale(2), Double.parseDouble("0.02"),
+        Long.parseLong("7364984"));
+    SummarySale summarySaleTwo =
+        new SummarySale(BigDecimal.valueOf(8.00).setScale(2), BigDecimal.valueOf(10.00).setScale(2),
+            Double.parseDouble("0.02"), Long.parseLong("7364985"));
+
+    summarys = new ArrayList<>();
+    summarys.add(summarySaleOne);
+    summarys.add(summarySaleTwo);
+
+  }
+
+  @Test
+  public void shouldFindAll() {
+    BDDMockito.given(repository.findAll()).willReturn(summarys);
+
+    service.findAll();
+
+    BDDMockito.verify(repository).findAll();
+  }
+
+}
